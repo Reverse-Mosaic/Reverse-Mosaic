acd5bd91908792f62569c11b7d3afe2b370bdcf7
James Stevenson <28961606+user1342@users.noreply.github.com>
2024-05-17 08:12:17 +0100

    Update flaw_finder_tool.py

--- ReverseMosaic/tool_hub/tools/flaw_finder_tool/flaw_finder_tool.py
+++ ReverseMosaic/tool_hub/tools/flaw_finder_tool/flaw_finder_tool.py
@@ -6,25 +6,28 @@ from llama_index.core.tools import FunctionTool
 import subprocess
 import sys
 import tempfile
-
+import os
 
 class FlawFinderTool(BaseToolClass):
     """A tool for interacting with FlawFinder."""
 
     @staticmethod
-    def find_flaws_and_vulnerabilities(c_code: str) -> str:
+    def find_flaws_and_vulnerabilities(code: str) -> str:
         """Retrieves information on weaknesses, issues, flaws, and vulnerabilities from provided decompiled C code.
 
         Args:
-            c_code (str): C decompilation
+            code (str): C decompilation code.
 
         Returns:
             str: A string containing information about identified issues.
         """
 
+        if os.path.isfile(code):
+            return "Binary file provided. First decompile a specific function to C."
+
         with tempfile.NamedTemporaryFile(mode='w', delete=False) as temp_file:
             # Write the string to the temporary file
-            temp_file.write(c_code)
+            temp_file.write(code)
             # Get the filename of the temporary file
             temp_file_name = temp_file.name
             python_executable_path = sys.executable
